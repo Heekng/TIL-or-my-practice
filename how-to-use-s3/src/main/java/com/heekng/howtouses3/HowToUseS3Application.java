636261c4b3ce61486cf3bdeda7c702dcb180f83c
heekng <rhkd1769@gmail.com>
2024-12-18 16:31:41 +0900

    init

---
+++ how-to-use-s3/src/main/java/com/heekng/howtouses3/HowToUseS3Application.java
@@ -0,0 +1,13 @@
+package com.heekng.howtouses3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HowToUseS3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HowToUseS3Application.class, args);
+    }
+
+}
